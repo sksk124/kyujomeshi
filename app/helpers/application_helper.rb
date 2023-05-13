fbdb4b6849859f95399b9cc27ca373281ef4a983
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-05-13 15:59:34 +0000

    Update review index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def star_rating(rating)
+    case rating
+    when 5
+      '⭐️⭐️⭐️⭐️⭐️'
+    when 4
+      '⭐️⭐️⭐️⭐️'
+    when 3
+      '⭐️⭐️⭐️'
+    when 2
+      '⭐️⭐️'
+    when 1
+      '⭐️'
+    else
+      ''
+    end
+  end
+
 end
