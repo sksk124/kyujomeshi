61198a973872a46ea0423477eee6d7a5a09e3208
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-05-16 15:04:23 +0000

    Update customer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,15 +1,22 @@
 class Public::CustomersController < ApplicationController
+ before_action :is_matching_login_customer, only: [:edit, :update]
+
+
   def show
+   @customer = Customer.find(params[:id])
   end
 
   def edit
+   is_matching_login_customer
+   @customer = Customer.find(params[:id])
   end
 
   def update
+   is_matching_login_customer
     @customer = Customer.find(params[:id])
    if @customer.update(customer_params)
     flash[:success] = "顧客情報を更新しました"
-    redirect_to admin_customer_path(@customer)
+    redirect_to customer_path(@customer)
    else
     flash.now[:error] = "顧客情報の更新に失敗しました"
     render :edit
@@ -18,6 +25,13 @@ class Public::CustomersController < ApplicationController
 
 
   private
+
+   def is_matching_login_customer
+    customer = Customer.find(params[:id])
+    unless customer.id == current_customer.id
+      redirect_to top_path
+    end
+   end
 
    def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :nick_name, :is_deleted)
