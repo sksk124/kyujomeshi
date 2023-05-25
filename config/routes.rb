a4d7d66c5a9bf9c7fad250c43ccf1b5783224881
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-05-25 13:06:19 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 
-devise_scope :customers do
-    post 'customers/guest_sign_in', to: 'customers/sessions#new_guest'
-  end
 
 # 管理者用
 # URL /admin/sign_in ...
@@ -19,6 +16,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
   get '/', to: 'public/homes#top', as: 'top'
   get '/about', to: 'public/homes#about', as: 'about'
+  post '/homes/guest_sign_in', to: 'public/homes#guest_sign_in'
 
 
 
@@ -34,6 +32,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     resources :reviews
     resources :comments
   end
+
 
 
 end
