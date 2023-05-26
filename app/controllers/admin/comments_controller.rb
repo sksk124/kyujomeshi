769981f66dfddabca52a1d969610f6d1ac91db59
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-05-26 14:16:46 +0000

    Make admin/review show

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def create
     @comment = Comment.new(comment_params)
     review = @comment.review
@@ -9,30 +9,29 @@ class Admin::CommentsController < ApplicationController
     else
       flash[:error] = "コメントの投稿に失敗しました"
     end
-    redirect_to review_path(review)
+    redirect_to admin_review_path(review)
   end
-
 
   def destroy
     @comment = Comment.find(params[:id])
-    review = @comment.review # 削除後のリダイレクト先のために関連するレビューを取得する
-
+    review = @comment.review
     if @comment.destroy
       flash[:success] = "コメントを削除しました"
     else
       flash[:error] = "コメントの削除に失敗しました"
     end
-
-    redirect_to review_path(review)
+    redirect_to admin_review_path(review)
   end
-
 
   private
 
-  def authenticate_admin!
-    unless admin_signed_in?
-    redirect_to top_path
-    end
+  def comment_params
+    params.require(:comment).permit(:comment, :customer_id, :review_id)
   end
 
+  def authenticate_admin!
+    unless admin_signed_in?
+      redirect_to top_path
+    end
+  end
 end
