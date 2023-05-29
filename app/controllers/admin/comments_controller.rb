c77ab5b854524904b481197d02849aa863d76a2a
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-05-29 04:51:54 +0000

    Update admin/review show

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,16 +1,6 @@
 class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
 
-  def create
-    @comment = Comment.new(comment_params)
-    review = @comment.review
-    if @comment.save
-      flash[:success] = "コメントが投稿されました"
-    else
-      flash[:error] = "コメントの投稿に失敗しました"
-    end
-    redirect_to admin_review_path(review)
-  end
 
   def destroy
     @comment = Comment.find(params[:id])
@@ -26,7 +16,7 @@ class Admin::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:comment, :customer_id, :review_id)
+  params.require(:comment).permit(:comment, :customer_id, :review_id)
   end
 
   def authenticate_admin!
