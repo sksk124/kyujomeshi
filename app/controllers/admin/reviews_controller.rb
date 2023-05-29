509381f067bd0369bf00aae6f260290507c72ad4
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-05-29 13:13:45 +0000

    Make admin/review show

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ReviewsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   def index
     @reviews = Review.all
@@ -7,14 +8,12 @@ class Admin::ReviewsController < ApplicationController
 
   def show
     @ballparks = Ballpark.all
-    @review = Review.find(params[:id])
     @comments = @review.comments
 
     # 投稿者での絞り込み
-   if params[:customer_id].present?
-     @reviews = @reviews.where(customer_id: params[:customer_id])
-   end
-
+    if params[:customer_id].present?
+      @reviews = @reviews.where(customer_id: params[:customer_id])
+    end
   end
 
   def new
@@ -22,28 +21,41 @@ class Admin::ReviewsController < ApplicationController
   end
 
   def create
-   @review = Review.new(review_params)
-   if @review.save
-    flash[:success] = 'レビューを投稿しました'
-    redirect_to admin_reviews_path
-   else
-    flash.now[:danger] = 'レビューの投稿に失敗しました'
-    render :new
-   end
+    @review = Review.new(review_params)
+    if @review.save
+      flash[:success] = 'レビューを投稿しました'
+      redirect_to admin_reviews_path
+    else
+      flash.now[:danger] = 'レビューの投稿に失敗しました'
+      render :new
+    end
   end
 
+  def edit
+    # 編集画面の表示
+  end
 
+  def update
+    if @review.update(review_params)
+      redirect_to admin_review_path(@review), notice: "レビューが更新されました。"
+    else
+      render :edit
+    end
+  end
 
   private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
 
   def review_params
     params.require(:review).permit(:image, :name, :price, :rating, :thought, :customer_id, :ballpark_id)
   end
 
   def authenticate_admin!
-   unless admin_signed_in?
-    redirect_to top_path
-   end
+    unless admin_signed_in?
+      redirect_to top_path
+    end
   end
-
 end
