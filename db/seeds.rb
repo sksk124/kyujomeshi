26ebb2aac2eceb1431352a5668b6bfb02a60bfc1
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-06-10 11:12:26 +0000

    Update testdata

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 # db/seeds.rb
 customers = Customer.create!(
   [
-    { id: 0, last_name: '管理者', first_name: '管理者', last_name_kana: 'カンリシャ', first_name_kana: 'カンリシャ', email: 'admin@example.com', password: '01248929', nick_name: '管理者', is_deleted: false },
-    { id: 1, last_name: '球場', first_name: '太郎', last_name_kana: 'キュウジョウ', first_name_kana: 'タロウ', email: 'kyujo1@example.com', password: '12345678', nick_name: '球場くん', is_deleted: false },
-    { id: 2, last_name: '球場', first_name: '花子', last_name_kana: 'キュウジョウ', first_name_kana: 'ハナコ', email: 'kyujo2@example.com', password: '23456789', nick_name: '球場さん', is_deleted: false },
+    { last_name: '管理者', first_name: '管理者', last_name_kana: 'カンリシャ', first_name_kana: 'カンリシャ', email: 'admin@example.com', password: '01248929', nick_name: '管理者', is_deleted: false },
+    { last_name: '球場', first_name: '太郎', last_name_kana: 'キュウジョウ', first_name_kana: 'タロウ', email: 'kyujo1@example.com', password: '12345678', nick_name: '球場くん', is_deleted: false },
+    { last_name: '球場', first_name: '花子', last_name_kana: 'キュウジョウ', first_name_kana: 'ハナコ', email: 'kyujo2@example.com', password: '23456789', nick_name: '球場さん', is_deleted: false },
   ]
 )
 
