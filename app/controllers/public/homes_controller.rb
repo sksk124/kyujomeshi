cbf83c6d765883634be5137d29580093e211b041
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-07-07 05:22:46 +0000

    Update layout

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,31 +3,6 @@ class Public::HomesController < ApplicationController
 
   def top
     @reviews = Review.all
-
-  # 名前による検索
-   if params[:search].present?
-    @reviews = @reviews.where("name LIKE ?", "%#{params[:search]}%")
-   end
-
-  # 評価の絞り込み
-   if params[:rating].present?
-    @reviews = @reviews.where(rating: params[:rating])
-   end
-
-  # 金額の範囲での絞り込み
-   if params[:price].present?
-    case params[:price]
-    when "1"
-      @reviews = @reviews.where(price: 0..999)
-    when "2"
-      @reviews = @reviews.where(price: 1000..1999)
-    when "3"
-      @reviews = @reviews.where(price: 2000..2999)
-    when "4"
-      @reviews = @reviews.where("price >= ?", 3000)
-    end
-   end
-
     @ballparks = Ballpark.all
   end
 
