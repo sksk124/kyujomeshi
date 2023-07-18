2a80c83b735fe71b985c927ca399394749b816f0
EC2 Default User <125953174+sksk124@users.noreply.github.com>
2023-07-18 13:35:03 +0000

    Update timezone

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module Kyujomeshi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
-
-config.time_zone = 'Tokyo'
